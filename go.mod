95ed3f7d9f6f15fef89f14821321a49a4e1d4da4
JohnEsleyer <ralphesleyer@gmail.com>
2023-09-03 20:00:37 +0800

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/johnesleyer/websocket-go
+
+go 1.18
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println("Client connected")
+	conn.Close()
+}
