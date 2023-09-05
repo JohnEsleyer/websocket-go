ccea6807a34f3130c7d17b6376697f0f7c96a153
JohnEsleyer <ralphesleyer@gmail.com>
2023-09-05 10:33:47 +0800

    ADD: added manager.go

--- main.go
+++ main.go
@@ -1,44 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println("Error upgrading connection:", err)
-		return
-	}
-	defer conn.Close()
-
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println("Error reading message:", err)
-			return
-		}
-
-		fmt.Printf("Received message: %s\n", p)
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			fmt.Println("Error writing message:", err)
-			return
-		}
-	}
-}
-
 func main() {
-	http.HandleFunc("/ws", handleWebSocket)
-	fmt.Println("WebSocket server is running on :8080...")
-	http.ListenAndServe(":8080", nil)
+	setupAPI()
+
+	// Serve on port :8000, fudge yeah hardcoded port
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+// setupAPI will start all Routes and their Handlers
+func setupAPI() {
+	// Create a Manager instance used to handle WebSocket Connections
+	manager := NewManager()
+
+	// Serve the ./public directory at Route /
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/ws", manager.serveWS)
 }
